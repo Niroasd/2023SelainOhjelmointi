73ab14dc49fdcf113b1a50e4820a83e2ade00666
Niroasd <nico.roininen@edu.turkuamk.fi>
2023-09-18 08:41:19 +0300

    initial commit.

---
+++ week2task1.js
@@ -0,0 +1,14 @@
+
+
+function isLeapYear(year) {
+    if (year % 4 === 0 && (year % 100 !== 0) || (year % 400 === 0)){
+        return true;
+    }
+    return false;
+}
+
+function isLeapYearShorter(year) {
+    return (year % 4 === 0 && (year % 100 !== 0) || (year % 400 === 0));
+}
+
+let arr = ['asd', 2, ['a', 'b']];
